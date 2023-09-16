cb1d529ac116b223af8cf268ec9892c102e6bf09
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-16 15:16:57 -0300

    chore: Add routes and cors in server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from "fastify"
-import { prisma } from "./lib/prisma"
+import { fastifyCors } from "@fastify/cors"
+import { getAllPromptsRoute } from "./routes/get-all-prompts"
+import { uploadVideoRoute } from "./routes/upload-video"
+import { createTranscriptionRoute } from "./routes/create-transcription"
+import { generateAICompletionRoute } from "./routes/generate-ai-completion"
 
 const app = fastify()
 
-app.get('/prompts', async () => {
-  const prompts = await prisma.prompt.findMany()
-
-  return prompts
+app.register(fastifyCors, {
+  origin: '*',
 })
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAICompletionRoute)
 
 app.listen({
   port: 3333,
